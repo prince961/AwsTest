40fc60e727af7c6e5510be804e1acd79b03b4380
prince961 <prince.prince961@gmail.com>
2019-05-12 09:32:06 +0530

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,13 @@ const router = express.Router();
 const User = require('../models/user');
 
 //get a list of ninjas from database
-router.get('/ninjas',function(req,res){
+
+//router.get('/ninjas',function(req,res){
+  //  res.send({type: 'GET'});
+    //});
+    
+
+router.get('/ninjas/:id',function(req,res){
 User.findOne({emailId: req.params.id}).then(function(user){
     res.send(user);
 });
